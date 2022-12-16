825c8e57d011a1b0dd6236338882ce11bd77af77
Tim <timothee.varaillas@gmail.com>
2022-12-16 16:06:16 +0100

    @ modifiying layout

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,49 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from "vue-router";
+import { ref } from "vue";
+const activeIndex = ref("0");
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <el-menu
+      :default-active="activeIndex"
+      class="menu"
+      mode="horizontal"
+      router="true"
+    >
+      <img
+        alt="Yescapa logo"
+        class="logo"
+        src="@/assets/yescapa.png"
+        height="50"
+      />
+      <div class="flex-grow" />
+      <el-menu-item index="0" route="/">Home</el-menu-item>
+      <el-menu-item index="1" route="/about">About</el-menu-item>
+    </el-menu>
   </header>
 
-  <RouterView />
+  <div class="container">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.menu {
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
 }
-
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  margin-right: 30px;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+a {
+  color: inherit;
+  text-decoration: inherit;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.container {
+  width: 1170px;
+  margin: 0 auto;
 }
 </style>
