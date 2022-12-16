0a2849e22aceeb75d3a062a4e489c19949c19783
Tim <timothee.varaillas@gmail.com>
2022-12-16 22:14:50 +0100

    + creating VehiclesListView

--- src/views/VehiclesListView.vue
+++ src/views/VehiclesListView.vue
@@ -1,9 +1,33 @@
-<script setup lang="ts">
-import List from "../components/vehicle/List.vue";
-</script>
-
 <template>
   <main>
-    <List />
+    <List :vehicles="vehicles" />
   </main>
 </template>
+
+<script lang="ts">
+import List from "../components/vehicle/List.vue";
+import { defineComponent } from "vue";
+import axios from "axios";
+
+export default defineComponent({
+  components: {
+    List,
+  },
+  data() {
+    return {
+      vehicles: [] as any[],
+    };
+  },
+  methods: {
+    async fetchVehicles() {
+      const response = await axios.get<any>(
+        "https://gitlab.com/api/v4/snippets/2095016/raw"
+      );
+      this.vehicles = response.data.results;
+    },
+  },
+  async mounted() {
+    await this.fetchVehicles();
+  },
+});
+</script>
